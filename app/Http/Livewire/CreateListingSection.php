61f923afac13b28ad2d77b44215362cc47b1ff81
Warra <warrenmanley@gmail.com>
2022-07-05 13:59:26 +0200

    feat(create-listing): [wip] create listing component

--- app/Http/Livewire/CreateListingSection.php
+++ app/Http/Livewire/CreateListingSection.php
@@ -3,15 +3,65 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Listing;
+use App\Models\Category;
+use Illuminate\Support\{Str, Arr};
+use Carbon\Carbon;
 
 class CreateListingSection extends Component
 {
     public $title;
     public $description;
-    public $category;
+    public $descriptionCount;
+    public $categories;
+    public $categorySelected;
     public $amount;
     public $mobile;
     public $email;
+
+    protected $rules = [
+        'email' => 'required|email',
+        'mobile' => 'required|numeric|digits:10',
+        'message' => 'required|max:300',
+    ];
+
+    public function mount()
+    {
+        $this->categories = Category::all()->toArray();
+        $this->categorySelected = 0;
+        $this->descriptionCount = 800;
+    }
+
+    public function updatedDescription()
+    {
+        $this->descriptionCount = 800 - strlen($this->description);
+    }
+
+    public function createListing()
+    {
+        $this->validate();
+
+        $selectedCategory = Arr::first($this->categories, function ($value, $key) {
+            return $value['id'] = $this->categorySelected;
+        });
+
+        $uuid = Str::uuid();
+
+        $listing = new Listing();
+        $listing['uuid'] = $uuid;
+        $listing['title'] = $this->title;
+        $listing['description'] = $this->description;
+        $listing['slug'] = '\/listings\/'.$category.'\/'.$uuid;
+        $listing['online_at'] = Carbon::now()->toDateTimeString();
+        $listing['offline_at'] = null;
+        $listing['amount'] = $this->amount;
+        $listing['currency'] = 'ZAR';
+        $listing['email'] = $this->email;
+        $listing['mobile'] = $this->mobile;
+        $listing['category_id'] = $selectedCategory['id'];
+        
+        $listing->save();
+    }
 
     public function render()
     {
--- resources/views/livewire/create-listing-section.blade.php
+++ resources/views/livewire/create-listing-section.blade.php
@@ -1,28 +1,39 @@
-<div class="flex flex-col w-full h-fit mt-24 px-24">
+<div class="flex flex-col w-full h-fit mt-24 px-48">
     <div class="border-solid border-2 border-gray-300 p-8">
         <div class="flex text-xl">
-            Create Listing:
+            Listing Details:
         </div>
-        <div>
-            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Title" type="text" wire:model="title" />
+        <div class="mt-4">
+            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-1/3 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Title" type="text" wire:model="title" />
             @error('title') <span class="text-red-500">{{ $message }}</span> @enderror
         </div>
         <div class="mt-4">
-            <textarea class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Message" type="text" rows="5" wire:model="message" maxlength="300"></textarea>
-            @error('message') <span class="text-red-500">{{ $message }}</span> @enderror
-            <div class="text-sm text-grey-300 mt-2">{{$messageCount}} characters left</div>
+            <textarea class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Description" type="text" rows="5" wire:model="description" maxlength="300"></textarea>
+            @error('description') <span class="text-red-500">{{ $message }}</span> @enderror
+            <div class="text-sm text-grey-300 mt-2">{{$descriptionCount}} characters left</div>
         </div>
-        <div>
-            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Email" type="text" wire:model="email" />
+        <div class="mt-4">
+            <select wire:model="categorySelected" class="placeholder:italic placeholder:text-slate-400 block bg-white w-2/4 md:w-1/4 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">
+                <option value=0>Select Category</option>
+                @if($categories)
+                    @foreach($categories as $cat)
+                        <option value={{$cat['id']}}>{{$cat['name']}}</option>
+                    @endforeach
+                @endif
+            </select>
+        </div>
+        <div class="mt-4">
+            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-1/3 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Amount" type="text" wire:model="amount" />
+            @error('amount') <span class="text-red-500">{{ $message }}</span> @enderror
+        </div>
+        <div class="mt-4">
+            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-1/3 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Mobile" type="text" wire:model="mobile" />
+            @error('mobile') <span class="text-red-500">{{ $message }}</span> @enderror
+        </div>
+        <div class="mt-4">
+            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-1/3 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Email" type="text" wire:model="email" />
             @error('email') <span class="text-red-500">{{ $message }}</span> @enderror
         </div>
-        <div>
-            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Email" type="text" wire:model="email" />
-            @error('email') <span class="text-red-500">{{ $message }}</span> @enderror
-        </div>
-        <div>
-            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Email" type="text" wire:model="email" />
-            @error('email') <span class="text-red-500">{{ $message }}</span> @enderror
-        </div>
+        <button class="inline-flex items-center mt-4 px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-maingold bg-mainblue" wire:click="createListing">Create Listing</button>
     </div>
 </div>
