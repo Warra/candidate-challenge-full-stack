1fdc1a9ee490d0d5142c842a93da146b28265c59
Warra <warrenmanley@gmail.com>
2022-07-06 10:41:47 +0200

    feat(loading-state): Adds loading state with 1 second sleep to show loader

--- resources/views/livewire/listing-results.blade.php
+++ resources/views/livewire/listing-results.blade.php
@@ -1,4 +1,9 @@
 <div class="my-10">
+    <div wire:loading>
+        <div class="absolute top-1/2 mt-8 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+            <img src="/icons/loader.svg" />
+        </div>
+    </div>
     <div class="text-2xl text-gray-500">
         @if($query === "")
             Recent Listings:
@@ -10,17 +15,19 @@
             Search Results:
         @endif
     </div>
-    @if(count($listings) > 0)
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 place-content-start my-10">
-            @foreach($listings as $key => $listing)
-                <div class="flex w-full h-50 bg-white rounded-md shadow-md z-20">
-                    <a href="{{$listing['slug']}}" class="flex flex-col py-4 px-4">
-                        <div class="flex text-bold text-2xl text-mainblue text-bold">{{$listing["title"]}}</div>
-                        <div class="text-black text-xl">R{{$listing["amount"]}}</div>
-                        <div class="flex h-full mt-4 overflow-hidden break-all font-normal text-base">{{$listing["description"]}}</div>
-                    </a>
-                </div>
-            @endforeach
-        </div>
-    @endif
+    <div wire:loading.remove>
+        @if(count($listings) > 0)
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 place-content-start my-10">
+                @foreach($listings as $key => $listing)
+                    <div class="flex w-full h-50 bg-white rounded-md shadow-md z-20">
+                        <a href="{{$listing['slug']}}" class="flex flex-col py-4 px-4">
+                            <div class="flex text-bold text-2xl text-mainblue text-bold">{{$listing["title"]}}</div>
+                            <div class="text-black text-xl">R{{$listing["amount"]}}</div>
+                            <div class="flex h-full mt-4 overflow-hidden break-all font-normal text-base">{{$listing["description"]}}</div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @endif
+    </div>
 </div>
